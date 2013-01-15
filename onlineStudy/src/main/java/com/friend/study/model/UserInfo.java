38d225c0210a7e2b06cd023c83425ff2bb284e78
ycx211314 <ycx211314@gmail.com>
2013-01-15 09:44:27 +0800

    create the first class

---
+++ onlineStudy/src/main/java/com/friend/study/model/UserInfo.java
@@ -0,0 +1,14 @@
+package com.friend.study.model;
+
+import java.io.Serializable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13-1-15
+ * Time: 上午9:37
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserInfo implements Serializable {
+    private Integer userId;
+}
